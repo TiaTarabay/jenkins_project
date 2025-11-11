c568aba2b262979c749c51baa0409f1de99bec30
TiaTarabay <twt00@mail.aub.edu>
2025-11-11 11:58:06 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,14 @@
- # tests/test_app.py
-from os import name
+# tests/test_app.py
 import unittest
+import os
+import sys
+sys.path.append(os.getcwd())
+
 from app import greet
+
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"),"Hello, World fromTia Tarabay!")
+        self.assertEqual(greet("World"), "Hello, World from Tia Tarabay!")
 
-if name == "__main__":
+if __name__ == "__main__":
     unittest.main()
